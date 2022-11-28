63ac3e250fcd2a2df46599a264557f8a9b5abffd
MyNzzz <104083526+is-MyNzzz@users.noreply.github.com>
2022-11-28 12:47:26 +0300

    Add files via upload

---
+++ src/scripts/change.js
@@ -0,0 +1,26 @@
+var theme = "off";
+
+function change() {
+  var root = document.documentElement;
+  var btn = document.getElementById("btn");
+
+  if ( theme == "off" ) {
+    root.style.setProperty("--background-color", "white");
+    root.style.setProperty("--button-color", "black");
+
+    btn.classList.add("fa-toggle-on");
+    btn.classList.remove("fa-toggle-off");
+
+    document.title = "ВКЛЮЧЕНО";
+    theme = "on";
+  } else {
+    root.style.setProperty("--background-color", "black");
+    root.style.setProperty("--button-color", "white");
+
+    btn.classList.add("fa-toggle-off");
+    btn.classList.remove("fa-toggle-on");
+
+    document.title = "ВЫКЛЮЧЕНО";
+    theme = "off";
+  }
+}
